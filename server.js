c50ef8b10ccb81dbee20d5b428427382c2fbfadd
Maria <msawaby@gmail.com>
2017-07-02 22:50:04 -0700

    heroku connection added

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ var express = require("express");
 var bodyParser = require("body-parser");
 var methodOverride = require("method-override");
 var PORT = process.env.PORT || 3000;
+
 var app = express();
 // Requiring our models for syncing
 var db = require("./models");
